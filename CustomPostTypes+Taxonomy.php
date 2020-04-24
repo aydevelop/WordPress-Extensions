938ef7492ec8cf389cf75dedb9b07355786249db
aydevelop <ay.develop1@gmail.com>
2020-04-24 15:46:08 +0300

    Metadata

---
+++ CustomPostTypes+Taxonomy.php
@@ -0,0 +1,13 @@
+<?php 
+/*
+    Plugin Name: CustomPostTypes+Taxonomy
+    Plugin URI: http://juwric.info
+    Description: Pellentesque et tellus gravida, finibus risus sed, sagittis orci. Praesent ac nibh varius, lacinia purus ut, euismod purus. Maecenas mattis molestie ante vitae dapibus. Curabitur et aliquet lectus.
+    Author: juwric
+    Author URI: http://juwric.info
+    Version: 1.0.0
+    Text Domain: juwric
+    Licence: GPL
+*/
+
+

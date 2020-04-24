02a421d2f8d8d73de13ec0ac04ff05cbea2b8698
aydevelop <ay.develop1@gmail.com>
2020-04-24 17:15:52 +0300

    Registered  Taxonomy

--- register.php
+++ register.php
@@ -1,6 +1,6 @@
 <?php 
 
-function cptt_register_post_type() {
+function cptt_register_post_type_book() {
 	$labels = array(
 		'name' => __("Books", "cptt"),
 		'singular_name' => __("Book", "cptt"),
@@ -21,9 +21,32 @@ function cptt_register_post_type() {
 			'title',
 			'editor',
 			'excerpt',
-			'page-attributes'
+			'page-attributes',
+			'taxonomies' => array('genres'),
 		)
 	);
-	register_post_type('books', $args);
+    register_post_type('book', $args);    
 }
-add_action( 'init' , 'cptt_register_post_type' );
+add_action( 'init' , 'cptt_register_post_type_book' );
+
+function cptt_register_taxonomy_genres() {
+	$labels = array(
+		'name' => __( 'Genres', 'cptt' ),
+		'singular_name' => __ ( 'Genre', 'cptt' ),
+		'search_items' => __( 'Search Genres', 'cptt' ),
+		'all_items' => __( 'All Genres', 'cptt' ),
+		'edit_item' => __( 'Edit Genre', 'cptt' ),
+		'update_item' => __( 'Update Genre', 'cptt' ),
+		'add_new_item' => __( 'Add New Genre', 'cptt' ),
+		'new_item_name' => __( 'New Genre Name', 'cptt' ),
+		'menu_name' => __('Genres', 'cptt' )
+	);
+	$args = array(
+		'hierarchical' => true,
+		'rewrite' => array( 'slug' => 'genres' ),
+		'show_admin_column' => true,
+		'labels' => $labels
+	);
+    register_taxonomy( 'genres', 'book', $args );    
+}
+add_action( 'init' , 'cptt_register_taxonomy_genres' );

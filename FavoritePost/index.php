01678193533d671a3399d9dbaee4d4724f8c5354
aydevelop <ay.develop1@gmail.com>
2020-04-25 11:12:48 +0300

    2. Init "FavoritePost"

---
+++ FavoritePost/index.php
@@ -0,0 +1,11 @@
+<?php 
+/*
+    Plugin Name: FavoritePost
+    Plugin URI: https://github.com/aydevelop
+    Description: Add posts to favorites
+    Author: juwric
+    Author URI: http://juwric.info
+    Version: 1.0.0
+    Text Domain: frp
+    Licence: GPL
+*/

bbad7741a093acc04c7fe9442e2f0b0d446a479d
aydevelop <ay.develop1@gmail.com>
2020-04-25 12:36:32 +0300

    2. Added style and script

--- FavoritePost/index.php
+++ FavoritePost/index.php
@@ -20,3 +20,9 @@ function frp_add_post_link ( $content ) {
     return $content;
 }
 add_action( 'the_content' , 'frp_add_post_link' );
+
+function frp_enqueue_scripts_styles ( $content ) {
+    wp_enqueue_script( 'frp_script', plugins_url("script.js", __FILE__), array('jquery'), null, true );
+    wp_enqueue_style( 'frp_style', plugins_url("style.css", __FILE__) );
+}
+add_action('wp_enqueue_scripts', 'frp_enqueue_scripts_styles');

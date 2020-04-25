c7c818e1b243717643581dce11aacc8a2e31ad42
aydevelop <ay.develop1@gmail.com>
2020-04-25 15:41:23 +0300

    2. Ajax click

--- FavoritePost/index.php
+++ FavoritePost/index.php
@@ -13,9 +13,10 @@
 function frp_add_post_link ( $content ) {
     if( is_user_logged_in() ){
         if( is_single() || is_home() ){
-            return "<p><a href='#'>&#9733; ".__( "Add to favourites", "frp" )." &#9733;</a></p>" . $content;
+            return "<p><a class='frp_post_link' href='#'>&#9733; <span> ".__( "Add to favourites", "frp" )."</span> &#9733;</a></p>" . $content;
         }        
         return $content;
+
     }
     return $content;
 }
@@ -24,5 +25,26 @@ add_action( 'the_content' , 'frp_add_post_link' );
 function frp_enqueue_scripts_styles ( $content ) {
     wp_enqueue_script( 'frp_script', plugins_url("script.js", __FILE__), array('jquery'), null, true );
     wp_enqueue_style( 'frp_style', plugins_url("style.css", __FILE__) );
+
+    global $post;
+    wp_localize_script( 'frp_script', 'frp_obj', 
+        array(
+                'url' => admin_url('admin-ajax.php'),
+                'nonce' => wp_create_nonce(),
+                'postId' => $post->ID
+            )
+    );
 }
 add_action('wp_enqueue_scripts', 'frp_enqueue_scripts_styles');
+
+function wp_ajax_wf_test () {
+    
+    if(!wp_verify_nonce( $_POST['nonce'] )){
+        echo "error";
+    }
+
+    echo $_POST["postId"];
+
+    wp_die();
+}
+add_action('wp_ajax_frp', 'wp_ajax_wf_test');
--- FavoritePost/script.js
+++ FavoritePost/script.js
@@ -1,1 +1,28 @@
-console.log("frp init");
+jQuery( document ).ready(function($) {
+
+    $('.frp_post_link').click(function(e){
+        e.preventDefault();
+        let _this = this;
+        $(_this).css("background-color", "pink");
+
+         $.ajax({
+             type: 'POST',
+             url: frp_obj.url,
+             data: {
+                 nonce: frp_obj.nonce,
+                 action: 'frp',
+                 postId: frp_obj.postId
+             },
+             success: function(res){
+                 console.log(res);
+             },
+             error: function(error){
+                 console.log("error " + error);
+             },
+             complete: function(){                
+                $(_this).css("background-color", "white");
+             }
+         });
+     });
+
+});

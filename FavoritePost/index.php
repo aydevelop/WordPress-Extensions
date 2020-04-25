8568423ea065d14bf6d212241c07cf94c6349706
aydevelop <ay.develop1@gmail.com>
2020-04-25 12:14:36 +0300

    2. Added post link

--- FavoritePost/index.php
+++ FavoritePost/index.php
@@ -9,3 +9,14 @@
     Text Domain: frp
     Licence: GPL
 */
+
+function frp_add_post_link ( $content ) {
+    if( is_user_logged_in() ){
+        if( is_single() || is_home() ){
+            return "<p><a href='#'>&#9733; ".__( "Add to favourites", "frp" )." &#9733;</a></p>" . $content;
+        }        
+        return $content;
+    }
+    return $content;
+}
+add_action( 'the_content' , 'frp_add_post_link' );
